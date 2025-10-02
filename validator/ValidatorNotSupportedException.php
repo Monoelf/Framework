85221bda031736989c9fc671a517f30675821680
monolit <v.monolit@efko.ru>
2025-10-02 21:34:14 +0300

    [MONOELF] wip: правки валидатора

--- validator/ValidatorNotSupportedException.php
+++ validator/ValidatorNotSupportedException.php
@@ -6,10 +6,4 @@ namespace Monoelf\Framework\validator;
 
 use RuntimeException;
 
-final class ValidatorNotSupportedException extends RuntimeException
-{
-    public function __construct(string $message)
-    {
-        parent::__construct("Валидатор для правила '{$message}' не задан");
-    }
-}
+final class ValidatorNotSupportedException extends RuntimeException {}
