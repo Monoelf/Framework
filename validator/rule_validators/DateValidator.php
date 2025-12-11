5ba19555d4773f89e28e5fe0762928bd9f20cf55
monolit <v.monolit@efko.ru>
2025-12-11 09:35:41 +0300

    [MONOELF] wip: правка валидатора

--- validator/rule_validators/DateValidator.php
+++ validator/rule_validators/DateValidator.php
@@ -13,6 +13,10 @@ final class DateValidator implements RuleValidatorInterface
 {
     public function validate(mixed $value, array $options = []): void
     {
+        if (is_string($value) === false || $value === '') {
+            throw new ValidationException($options['errorMessage'] ?? 'Значение должно быть строкой содержащей дату');
+        }
+
         if (isset($options['format']) === true) {
             if (
                 DateTime::createFromFormat($options['format'], $value) === false
